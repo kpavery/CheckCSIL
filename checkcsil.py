f721049b55fe65ca7cb405d22d499e272812e939
Keith Avery <kpavery@gmail.com>
2013-11-24 12:20:21 -0800

    Added username handling, as opt. argument. Fixes #1.
    You can now run the script with one argument, and that argument will be interpreted as your CSIL username. The argument is optinal, and if omitted the username will default to the local computer username running the script.

--- checkcsil.py
+++ checkcsil.py
@@ -5,7 +5,7 @@
 
 from paramiko import SSHClient, RejectPolicy
 from socket import timeout
-from sys import stdout
+from sys import stdout, argv
 
 workstations = ["snarf", "homer", "dilbert", "thundarr", "dudley", "cartman", "linus", "megatron", "marge", "whiley", "lupin", "elroy", "garfield", "bart", "butthead", "kyle", "scooby", "dagwood", "yogi", "mickey", "boris", "beavis", "popeye", "calvin", "aeonflux", "tygra", "taz", "pinky", "kenny", "snoopy", "dot", "speed", "akira", "tick", "optimus", "blondie", "tom", "booboo", "bugs", "eeyore", "bullwinkle", "brain", "sylvester", "racerx", "lisa", "hobbes", "wacko", "shaggy"]
 workstations.sort()
@@ -14,13 +14,17 @@ client = SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(RejectPolicy())
 
+username = None
+if len(argv) > 1:
+	username = argv[1]
+
 try:
 	for workstation in workstations:
 		try:
 			hostname = workstation + ".cs.ucsb.edu"
 			stdout.write(".")
 			
-			client.connect(hostname=hostname,timeout=2)
+			client.connect(hostname=hostname,username=username, timeout=2)
 			sshin, sshout, ssherr = client.exec_command("who")
 			lines = sshout.readlines()
 			client.close()
