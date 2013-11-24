434c76fced2620f8be44541da8c650365968742d
Keith Avery <kpavery@gmail.com>
2013-11-24 00:02:38 -0800

    Changed to specific imports, rather than module-wide imports.

--- checkcsil.py
+++ checkcsil.py
@@ -3,35 +3,35 @@
 ## By Keith Avery
 ## Copyright 2013, All Rights reserved
 
-import paramiko
-import socket
-import sys
+from paramiko import SSHClient, RejectPolicy
+from socket import timeout
+from sys import stdout
 
 workstations = ["snarf", "homer", "dilbert", "thundarr", "dudley", "cartman", "linus", "megatron", "marge", "whiley", "lupin", "elroy", "garfield", "bart", "butthead", "kyle", "scooby", "dagwood", "yogi", "mickey", "boris", "beavis", "popeye", "calvin", "aeonflux", "tygra", "taz", "pinky", "kenny", "snoopy", "dot", "speed", "akira", "tick", "optimus", "blondie", "tom", "booboo", "bugs", "eeyore", "bullwinkle", "brain", "sylvester", "racerx", "lisa", "hobbes", "wacko", "shaggy"]
 workstations.sort()
 
-client = paramiko.SSHClient()
+client = SSHClient()
 client.load_system_host_keys()
-client.set_missing_host_key_policy(paramiko.RejectPolicy())
+client.set_missing_host_key_policy(RejectPolicy())
 
 try:
 	for workstation in workstations:
 		try:
 			hostname = workstation + ".cs.ucsb.edu"
-			sys.stdout.write(".")
+			stdout.write(".")
 			
 			client.connect(hostname=hostname,timeout=2)
-			stdin, stdout, stderr = client.exec_command("who")
-			lines = stdout.readlines()
+			sshin, sshout, ssherr = client.exec_command("who")
+			lines = sshout.readlines()
 			client.close()
 			
 			if len(lines) <= 1:
 				print
-				sys.stdout.write(hostname)
+				stdout.write(hostname)
 				break
 			
-		except socket.timeout:
-			sys.stdout.write("T")
+		except timeout:
+			stdout.write("T")
 	
 	print
 except Exception, e:
